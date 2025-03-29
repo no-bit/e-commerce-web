a3235541502bf560660232879fbd71c9cbb69eb2
no-bit <sinanburakfiliz@gmail.com>
2025-03-29 22:25:02 +0300

    the other files

---
+++ src/main/java/com/ecommerce/burak/BurakApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.burak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BurakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BurakApplication.class, args);
+	}
+
+}
