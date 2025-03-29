2e369c96b8c1edb336b9fc19d647daf35b7cf3ad
no-bit <sinanburakfiliz@gmail.com>
2025-03-29 18:21:03 +0300

    Starting off my e-Commerce site

---
+++ src/test/java/com/ecommerce/burak/BurakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.burak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BurakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
